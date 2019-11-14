e0478d803629a98939d3e93937748c5918dbd60e
Claudio Ricci <claudioricci@Claudios-MBP.home>
2019-11-14 10:33:20 +0000

    working data fetch

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
 import Card from "../Card/Card";
 import LoadingCircle from "../LoadingCircle/LoadingCircle";
 import { getItem } from "../../methods/generalMethods";
@@ -7,70 +8,25 @@ import { pure } from "recompose";
 import "./Grid.scss";
 
 function Grid() {
-  const firstUpdate = useRef(false);
-  const [error, setError] = useState(null);
-  const [load, setLoad] = useState(false);
-  const [images, setImages] = useState(null);
-  const allImages = "http://www.splashbase.co/api/v1/images/search?query=cars";
-
-  console.table(allImages);
-
+  const [data, setData] = useState({ images: [] });
   useEffect(() => {
-    if (!firstUpdate.current) {
-      getItem(allImages)
-        .then(res => {
-          setImages(res);
-          setLoad(true);
-        })
-        .catch(err => {
-          setError(err);
-          setLoad(true);
-        });
-    }
-    firstUpdate.current = false;
+    const fetchData = async () => {
+      const result = await axios(
+        "http://www.splashbase.co/api/v1/images/search?query=cars"
+      );
+      setData(result.data);
+    };
+    fetchData();
   }, []);
-
-  if (load) {
-    return (
-      <main className="grid" data-testid="grid">
-        <div className="centered">
-          <section className="cards">
-            {error ? (
-              <h1>{error.message}</h1>
-            ) : (
-              images.map(image => {
-                const {
-                  id,
-                  url,
-                  large_url,
-                  source_id,
-                  copyright,
-                  site
-                } = image;
-                return (
-                  <Card
-                    key={uuid.v4()}
-                    id={id}
-                    url={url}
-                    large_url={large_url}
-                    source_id={source_id}
-                    copyright={copyright}
-                    site={site}
-                  />
-                );
-              })
-            )}
-          </section>
-        </div>
-      </main>
-    );
-  } else {
-    return (
-      <main data-testid="grid">
-        <LoadingCircle />
-      </main>
-    );
-  }
+  return (
+    <ul>
+      {data.images.map(item => (
+        <li key={item.id}>
+          <a href={item.url}>{item.site}</a>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default pure(Grid);
